b0a2d18c85e77fb22fbe41cb2fbd20788e73b5a4
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-06-29 15:29:21 +0300

    Added sign in button to loginviewcontroller to differentiate from other text fields

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
 
     let loginView = LoginView()
+    let signInButton = UIButton(type: .system)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +26,39 @@ extension LoginViewController {
     
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false // necessary for auto showing of view
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8    // for indicator spacing
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         
         NSLayoutConstraint.activate([   // constraint that shows where the view will be placed
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor) ,
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1) ,
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
+        
+        NSLayoutConstraint.activate([   // constraint that shows where the view will be placed
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 1)
+            // beneath login view
+        ])
+    }
+}
+
+// MARK: Actions
+
+extension LoginViewController {
+    @objc func signInTapped(sender: UIButton) {
+        
     }
 }
 
