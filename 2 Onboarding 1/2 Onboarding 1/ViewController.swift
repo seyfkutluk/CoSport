9e48506b81ee6f4b78fa2fa159066687989fa109
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-07-01 00:43:58 +0300

    Onboarding 1 is created

---
+++ 2 Onboarding 1/2 Onboarding 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Onboarding 1
+//
+//  Created by Seyfülmülük Kutluk on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
