a0a810740905cb5e3932b0f05e63c46c849f8a01
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-07-06 20:41:38 +0300

    [Bankey][Seyfulmuluk Kutluk][Added onboarding]

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = LoginViewController()  // refactor rename change name
+        //window?.rootViewController = LoginViewController()  // refactor rename change name
+        window?.rootViewController = OnboardingContainerViewController()
         
         return true
     }
