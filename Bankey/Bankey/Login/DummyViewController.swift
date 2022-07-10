fd82366de6038f169bd1d64952c103a93ae7d431
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-07-10 13:44:30 +0300

    [Seyfulmuluk Kutluk][logout page added]
    Signed-off-by: Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>

---
+++ Bankey/Bankey/Login/DummyViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DummyViewController.swift
+//  Bankey
+//
+//  Created by Seyfülmülük Kutluk on 10.07.2022.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController   {
+    let stackView = UIStackView()
+    let label = UILabel()
+    let logoutButton = UIButton(type: .system)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension DummyViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.configuration = .filled()
+        logoutButton.setTitle("Logout", for: [])
+        logoutButton.addTarget(self, action: #selector(logoutButton), for: .primaryActionTriggered)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(logoutButton)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func logoutButtonTapped(sender: UIButton){
+        // MARK: TODO
+    }
+}
