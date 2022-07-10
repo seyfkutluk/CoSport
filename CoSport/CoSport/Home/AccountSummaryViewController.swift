f9aaeed7b690fab39d4833a0d9a9a25ceb363f92
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-07-10 22:34:41 +0300

    [Seyfulmuluk Kutluk][styling cell view]
    Signed-off-by: Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>

--- CoSport/CoSport/Home/AccountSummaryViewController.swift
+++ CoSport/CoSport/Home/AccountSummaryViewController.swift
@@ -33,6 +33,10 @@ extension AccountSummaryViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+        tableView.rowHeight =  AccountSummaryCell.rowHeight
+        tableView.tableFooterView = UIView()
+        
         tableView.translatesAutoresizingMaskIntoConstraints =  false
         view.addSubview(tableView)
         
@@ -58,9 +62,11 @@ extension AccountSummaryViewController {
 
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = games[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
         return cell
+//        let cell = UITableViewCell()
+//        cell.textLabel?.text = games[indexPath.row]
+//        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
