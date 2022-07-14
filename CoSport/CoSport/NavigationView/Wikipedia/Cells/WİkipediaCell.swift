b2e9cf35ca45446b19408edc8f64a85771793970
Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>
2022-07-14 23:19:44 +0300

    [Seyfulmuluk Kutluk][added vikipedia view]
    Signed-off-by: Seyfülmülük Kutluk <seyfulmulukkutluk@Seyfulmuluks-MacBook-Pro.local>

---
+++ CoSport/CoSport/NavigationView/Wikipedia/Cells/WİkipediaCell.swift
@@ -0,0 +1,8 @@
+//
+//  WİkipediaCell.swift
+//  CoSport
+//
+//  Created by Seyfülmülük Kutluk on 14.07.2022.
+//
+
+import Foundation
---
+++ CoSport/CoSport/NavigationView/Wikipedia/WikipediaViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WikipediaViewController.swift
+//  CoSport
+//
+//  Created by Seyfülmülük Kutluk on 14.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class WikipediaViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBlue
+    }
+}
